03b3afb8fc43d385a228bb19041a91ba3d714b80
maggiejoe <maggiejoseph94@gmail.com>
2022-03-07 12:51:08 -0500

    shop inputs updated to integers instead of string

--- assets/js/game.js
+++ assets/js/game.js
@@ -136,23 +136,21 @@ var endGame = function() {
 var shop = function() {
     //as player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', OR 'LEAVE' to make a chioce."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
-
+    
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     // use swithc to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL": // new case
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
         
-        case "UPGRADE": // new case
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
 
-        case "LEAVE": // new case
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
 
             // do nothing, so function will end
